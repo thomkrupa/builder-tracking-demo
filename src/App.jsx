0dd372cde702d7ac9aa785ed642c677ca1c95543
thom krupa <thom@bejamas.io>
2024-06-13 13:32:40 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import { Content, fetchOneEntry, isPreviewing } from "@builder.io/sdk-react";
+import { useEffect, useState } from "react";
+
+// TODO: enter your public API key
+const BUILDER_PUBLIC_API_KEY = "f1a790f8c3204b3b8c5c1795aeac4660"; // ggignore
+
+function App() {
+  const [content, setContent] = useState(undefined);
+
+  useEffect(() => {
+    fetchOneEntry({
+      model: "page",
+      apiKey: BUILDER_PUBLIC_API_KEY,
+      userAttributes: {
+        urlPath: window.location.pathname || "/",
+      },
+    })
+      .then((content) => {
+        if (content) {
+          setContent(content);
+        }
+      })
+      .catch((err) => {
+        console.log(
+          "something went wrong while fetching Builder Content: ",
+          err,
+        );
+      });
+  }, []);
+
+  const shouldRenderBuilderContent = content || isPreviewing();
+
+  return shouldRenderBuilderContent ? (
+    <Content
+      content={content}
+      model="page"
+      apiKey={BUILDER_PUBLIC_API_KEY}
+      canTrack={false}
+    />
+  ) : (
+    <div>Content Not Found</div>
+  );
+}
+
+export default App;
